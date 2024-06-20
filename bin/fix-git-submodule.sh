b832b0ba29ce8d2654144517e7dd860df0ecb900
David Gómez Quilón <david.gomez.quilon@gmail.com>
2024-06-20 12:17:27 +0200

    [UPDATE] nginx

---
+++ bin/fix-git-submodule.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# This script updates the branch name to 'main'
+#
+# Source: https://github.com/evertramos/nginx-proxy-automation
+#
+
+# Get the script name and its real file path
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+SCRIPT_NAME="${0##*/}"
+CURRENT_PATH=$(pwd)
+
+cd "${SCRIPT_PATH}/../basescript"
+
+# Set submodules
+git submodule init
+git submodule update
+
+echo "Submodule is set"
+
+exit 0
+
